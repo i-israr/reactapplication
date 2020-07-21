a5194249b04d3b82f8dd70e7c0eaf19b678f0bfa
Israr ul haq <i-israr@github.com>
2020-07-21 15:05:36 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Israr from './israr.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Israr myName="Israr ul haq"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/israr.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Israr(props){
+    return(
+    <h1>My name is {props.myName}</h1>
+    )
+}
+
+export default Israr;
