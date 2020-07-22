40aee16c6515c739cc34041efc6030f48f2dabae
Israr ul haq <i-israr@github.com>
2020-07-22 22:39:09 +0500

    Session 4 updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Israr from './israr.js'
 
 function App() {
+  let [count, setCount] = useState(1);
+  let [isMorning, setMorning] = useState(true)
   return (
-    <div className="App">
-      <Israr myName="Israr ul haq"/>
+    <div className={`box ${isMorning ? 'dayLight' : ''}`}>
+      <Israr myName="Israr ul haq" />
+      
+      <h1>Good {isMorning ? 'Morning' : 'Night'}</h1>
+      
+      <h1>The counter is {count}</h1>
+      
+      <button onClick={() => setCount(count + 1)}>
+        Update Counter
+      </button>
+    
+      <button onClick={() => setMorning(!isMorning)}>
+        Update day
+      </button>
+
     </div>
   );
 }
