094780d042f8853f286f6b7ccce187a110b86d9c
Israr ul haq <i-israr@github.com>
2020-07-23 14:54:15 +0500

    Writing some commants for understanding

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
   let [count, setCount] = useState(1);
   let [isMorning, setMorning] = useState(true)
   return (
+    // <div className="box"> if calling single class
+    // If we want to write multiple line strings then we use `` e.g for concatination
+    // If we want to appand some veriable in `` then we use ${}
+    // If the isMorning will be true then the class day light will be called  
     <div className={`box ${isMorning ? 'dayLight' : ''}`}>
       <Israr myName="Israr ul haq" />
       
